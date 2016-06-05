2bfe797bc77a6826dd6b6e94b3799682e72e5520
Jiongxiao <tctjx@hotmail.com>
2016-06-05 17:19:13 +0800

    add toCovar
    Change-Id: I586743877a5e1c0121a9ecada23a8c335bcf57e1
    Signed-off-by: Jiongxiao <tctjx@hotmail.com>

---
+++ src/first_java/TestThread.java
@@ -0,0 +1,55 @@
+package first_java;
+
+
+public class TestThread {
+
+	public static void main(String[] args) {
+		//  Auto-generated method stub
+		//	Thread t = new MyThread(20);
+		//	t.start();
+		
+		MyTask myTask = new MyTask(20); //添加了一个线程
+		Thread t2= new Thread(myTask);
+		t2.start();
+		
+		for (int i = 0; i < 6; i++) {
+			System.out.println("Main--"+i);
+			try{
+				Thread.sleep(500);
+			}catch(InterruptedException e){}
+		}
+		
+	}
+
+}
+
+class MyThread extends Thread{
+	private int n;
+	public MyThread	(int n) {
+		super();
+		this.n=n;
+	}
+	public void run() {
+		for (int i = 0; i < n; i++) {
+			System.out.print(i+" ");
+		}
+		System.out.println();
+	}
+}
+
+
+class MyTask implements Runnable {
+	private int n;
+	public MyTask(int n) {
+		// TODO Auto-generated constructor stub
+		this.n=n;
+	}
+	public void run() {
+		for (int i = 0; i < n; i++) {
+			System.out.print(" "+i);
+			try{
+				Thread.sleep(500);
+			}catch(InterruptedException e){}
+		}
+	}
+}
